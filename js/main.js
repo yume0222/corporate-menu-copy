61b2fa256a2e8eb5bc3b772233da4a73d3728950
yume0222 <134505601+yume0222@users.noreply.github.com>
2025-06-07 19:04:56 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// スライドメニュー
+const header = document.querySelector('#header');
+const links = document.querySelectorAll('header a');
+const hamburger = document.querySelector('.hamburger');
+const mask = document.querySelector('#mask');
+hamburger.addEventListener('click', () => {
+  header.classList.toggle('open');
+  if (header.classList.contains('open')) {
+    header.classList.add('open');
+  } else {
+    header.classList.remove('open');
+  }
+});
+mask.addEventListener('click', () => {
+  header.classList.remove('open');
+});
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    header.classList.remove('open');
+  });
+});
+
+// スクロール
+const toTop = document.querySelector('#to-top');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 700) {
+    toTop.classList.add('show');
+  } else {
+    toTop.classList.remove('show');
+  }
+});
