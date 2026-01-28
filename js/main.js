674a74ea3517699eaa481508a12f768540abd949
yume0222 <134505601+yume0222@users.noreply.github.com>
2026-01-28 16:32:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // スライドメニュー
 const header = document.querySelector('#header');
 const links = document.querySelectorAll('header a');
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.header__hamburger');
 const mask = document.querySelector('#mask');
 hamburger.addEventListener('click', () => {
   header.classList.toggle('open');
